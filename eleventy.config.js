ab9ca684120bbf23a07e70dc491c3081c1cc7dbe
Casey Ocampo <caseysoflo@gmail.com>
2024-08-28 08:13:29 -0400

    chore: update homepage

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy({ "src/assets/js": "/assets/js" });
+  eleventyConfig.addPassthroughCopy({ "src/_public": "/" });
   eleventyConfig.addGlobalData("PROJECT_NAME", "A11y-oop");
 
   return {
